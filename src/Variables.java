309816daa43f20fdc8ede5ff824a26310bb6fc5b
Miguel <migueljomartinez@gmail.com>
2023-08-05 21:59:13 -0500

    First commit

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    void print() {
+        int salary = 5000;
+        System.out.println("Printing...");
+        String employeeName = "Miguel Martínez";
+        System.out.println(employeeName);
+    }
+}
