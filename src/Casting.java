4469d0725f9d78503419657652ac543645a5e170
Miguel <migueljomartinez@gmail.com>
2023-08-06 22:05:51 -0500

    Casting

---
+++ src/Casting.java
@@ -0,0 +1,9 @@
+public class Casting {
+    public static void main(String[] args) {
+        var monthlyDogs = 30.0 / 12;
+        System.out.println(monthlyDogs);
+        var truncatedDogs = (int) monthlyDogs;
+        System.out.println(truncatedDogs);
+        System.out.println((double) 30 / 12);
+    }
+}
