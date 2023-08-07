7afa0eff291b9fa5ec0da3aa684a1729bd738168
Miguel <migueljomartinez@gmail.com>
2023-08-06 20:58:58 -0500

    Number data types

---
+++ src/DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 999999999;
+        long nL = 999999999999999999L;
+
+        double nD = 123.99999999999999;
+        float nF = 123.999F;
+    }
+}
