a296f83b4a81d9bdc5fb7fbdc4adc00cf37a4999
Miguel <migueljomartinez@gmail.com>
2023-08-07 23:25:09 -0500

    Creating arrays

---
+++ src/collections/Arrays.java
@@ -0,0 +1,12 @@
+package collections;
+
+public class Arrays {
+	public static void main(String[] args) {
+		String[] androidVersions = new String[16];
+		String days[] = new String[7];
+		String[][] cities = new String[4][2];
+
+		int [][][] numbers = new int[10][5][2];
+		int [][][][][] n = new int[2][2][2][2][2];
+	}
+}
