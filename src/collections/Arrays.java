33507d78d9bd73dad107750618930003244373df
Miguel <migueljomartinez@gmail.com>
2023-08-07 23:27:45 -0500

    Creating array list

--- src/collections/Arrays.java
+++ src/collections/Arrays.java
@@ -1,5 +1,7 @@
 package collections;
 
+import java.util.ArrayList;
+
 public class Arrays {
 	public static void main(String[] args) {
 		String[] androidVersions = new String[16];
@@ -8,5 +10,13 @@ public class Arrays {
 
 		int [][][] numbers = new int[10][5][2];
 		int [][][][][] n = new int[2][2][2][2][2];
+
+		ArrayList names = new ArrayList();
+		names.add("Miguel");
+		names.add("Carmen");
+		names.add("Miguel A");
+		names.add("Andres");
+		names.add("Carolina");
+		names.add(1290);
 	}
 }
