58452b46a747bdadc3c65c984f4c316259cd7ff9
Miguel <migueljomartinez@gmail.com>
2023-08-10 23:44:08 -0500

    Creating doctor instance

--- src/oop/Doctor.java
+++ src/oop/Doctor.java
@@ -1,4 +1,11 @@
 package oop;
 
 public class Doctor {
+	int id;
+	String name;
+	String speciality;
+
+	public void showName() {
+		System.out.println(name);
+	}
 }
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -2,6 +2,8 @@ package oop;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("OOP Main class");
+		Doctor myDoctor = new Doctor();
+		myDoctor.name = "Andrés Martínez";
+		myDoctor.showName();
 	}
 }
