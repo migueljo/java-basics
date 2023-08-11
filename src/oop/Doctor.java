2d86af670417354d9bb8a5764d3119b2574ed598
Miguel <migueljomartinez@gmail.com>
2023-08-10 23:55:25 -0500

    Static properties

--- src/oop/Doctor.java
+++ src/oop/Doctor.java
@@ -1,19 +1,30 @@
 package oop;
 
+import javax.print.Doc;
+
 public class Doctor {
-	int id;
+	static int id = 0; // auto-incremented
+	private int instanceId;
 	String name;
 	String speciality;
 
 	Doctor() {
 		System.out.println("Building doctor object");
+		this.instanceId = Doctor.id;
+		Doctor.id++;
 	}
 
 	Doctor(String name) {
 		this.name = name;
+		this.instanceId = Doctor.id;
+		Doctor.id++;
 	}
 
 	public void showName() {
 		System.out.println("Doctor's name is: " + name);
+	}
+
+	public void showId() {
+		System.out.println("The id is: " + this.instanceId);
 	}
 }
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -3,6 +3,9 @@ package oop;
 public class Main {
 	public static void main(String[] args) {
 		Doctor myDoctor = new Doctor("Carolina Martínez");
+		Doctor myDoctor2 = new Doctor("Andrés Martínez");
 		myDoctor.showName();
+		myDoctor.showId();
+		myDoctor2.showId();
 	}
 }
