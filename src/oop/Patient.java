b22cecf93bf107167f9c8734ca153f714bfbaf52
Miguel <migueljomartinez@gmail.com>
2023-08-13 13:57:53 -0500

    Base structure for patient class

--- src/oop/Patient.java
+++ src/oop/Patient.java
@@ -1,4 +1,17 @@
 package oop;
 
 public class Patient {
+	String name;
+	String email;
+	String address;
+	String phoneNumber;
+	String birthday;
+	Double weight;
+	Double height;
+	String blood;
+
+	Patient(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
 }
