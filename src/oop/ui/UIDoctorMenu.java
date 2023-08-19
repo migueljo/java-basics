00b04f33231804189f915556165fe093a583ff72
Miguel <migueljomartinez@gmail.com>
2023-08-19 00:11:54 -0500

    showAddAvailableAppointmentsMenu part 2

--- src/oop/ui/UIDoctorMenu.java
+++ src/oop/ui/UIDoctorMenu.java
@@ -27,6 +27,27 @@ public class UIDoctorMenu {
 	}
 
 	public static void showAddAvailableAppointmentsMenu() {
+		var selectedMonth = 0;
+		do {
+			System.out.println();
+			System.out.println(":: Add available appointment");
+			System.out.println(":: Select a month");
+			for (int i = 0; i < 3; i++) {
+				System.out.println("Write " + (i + 1) + " to select " + UIMenu.MONTHS[i]);
+			}
+			System.out.println("0. Return");
 
+			Scanner sc = new Scanner(System.in);
+			selectedMonth = Integer.parseInt(sc.nextLine());
+
+			if (selectedMonth > 0 && selectedMonth < 4) {
+				System.out.println(selectedMonth + " -> " + UIMenu.MONTHS[selectedMonth]);
+				System.out.println("Insert the available date: [dd/mm/yyyy]");
+				String date = sc.nextLine();
+				System.out.println("Your date is: " + date + "\n1. Correct \n2.Change date");
+			} else if (selectedMonth == 0) {
+				showDoctorMenu();
+			}
+		} while(selectedMonth != 0);
 	}
 }
