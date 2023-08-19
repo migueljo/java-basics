17dc32e0f3d642f538f52a9244eece9ed795ecc3
Miguel <migueljomartinez@gmail.com>
2023-08-18 23:59:43 -0500

    Basic doctor menu

--- src/oop/ui/UIDoctorMenu.java
+++ src/oop/ui/UIDoctorMenu.java
@@ -1,4 +1,28 @@
 package oop.ui;
 
+import java.util.Scanner;
+
 public class UIDoctorMenu {
+	public static void showDoctorMenu() {
+		var response = 0;
+		do {
+			System.out.println("\n\n");
+			System.out.println("Doctor");
+			System.out.println("Welcome " + UIMenu.doctorLogged.getName());
+			System.out.println("1. Add available appointment");
+			System.out.println("2. My scheduled appointments");
+			System.out.println("0. Logout");
+
+			Scanner sc = new Scanner(System.in);
+			response = Integer.parseInt(sc.nextLine());
+
+			switch (response) {
+				case 1 -> {
+					System.out.println("Show available appointments");
+				}
+				case 2 -> System.out.println("Option 2");
+				case 3 -> UIMenu.showMenu();
+			}
+		} while (response != 0);
+	}
 }
