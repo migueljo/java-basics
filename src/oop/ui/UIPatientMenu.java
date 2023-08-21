8933613db7e0959fe492ad0958e298be4ca6b6a7
Miguel <migueljomartinez@gmail.com>
2023-08-20 23:53:08 -0500

    UIPatientMenu changes

--- src/oop/ui/UIPatientMenu.java
+++ src/oop/ui/UIPatientMenu.java
@@ -1,4 +1,33 @@
 package oop.ui;
 
+import java.util.Scanner;
+
 public class UIPatientMenu {
+	public static void showPatientMenu() {
+		var response = 0;
+		do {
+			System.out.println("Patient menu");
+			System.out.println("Welcome: " + UIMenu.patientLogged.getName());
+			System.out.println("1. Book an appointment");
+			System.out.println("2. My appointments");
+			System.out.println("0. Return");
+
+			Scanner sc = new Scanner(System.in);
+			response = Integer.parseInt(sc.nextLine());
+
+			switch (response) {
+				case 1 -> System.out.println("1");
+				case 2 -> System.out.println("2");
+				case 0 -> UIMenu.showMenu();
+			}
+		} while(response != 0);
+	}
+
+	private static void showBookAppointmentMenu() {
+		var response = 0;
+		do {
+			System.out.println(":: Book an appointment");
+			System.out.println(":: Select a date");
+		} while (response != 0);
+	}
 }
