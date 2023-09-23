c99f55e982a6bc5382efddb2f9ac20272c4c0f76
Miguel <migueljomartinez@gmail.com>
2023-09-23 07:00:25 -0500

    Centering window on the screen

--- src/learningjavabook/HelloJava.java
+++ src/learningjavabook/HelloJava.java
@@ -1,11 +1,22 @@
 package learningjavabook;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class HelloJava {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Hello Java!");
-		frame.setSize(300, 300);
+		JLabel label = new JLabel("Hello Java!", JLabel.CENTER);
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = toolkit.getScreenSize();
+		var width = 300;
+		var height = 300;
+		int x = (screenSize.width / 2) - (width / 2);
+		int y = (screenSize.height / 2) - (height / 2);
+		Rectangle frameBounds = new Rectangle(x, y, width, height);
+
+		frame.add(label);
+		frame.setBounds(frameBounds);
 		frame.setVisible(true);
 	}
 }
