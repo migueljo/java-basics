0f3d80822ac747765a1de766783b16af4fb662ca
Miguel <migueljomartinez@gmail.com>
2023-09-24 12:07:57 -0500

    Making label configurable

--- src/learningjavabook/chapter1/HelloJava2.java
+++ src/learningjavabook/chapter1/HelloJava2.java
@@ -5,8 +5,9 @@ import javax.swing.*;
 public class HelloJava2 {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("HelloJava2");
+		String label = args.length > 0 ? args[0] : "Hello world again :)";
 
-		frame.add(new HelloComponent2("Hello world again :)"));
+		frame.add(new HelloComponent2(label));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(300, 300);
 		frame.setVisible(true);
