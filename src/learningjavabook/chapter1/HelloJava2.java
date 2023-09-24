b59a0162b91b0748b9f1eb07cf724fc3f42a06f2
Miguel <migueljomartinez@gmail.com>
2023-09-24 11:42:35 -0500

    Show frame

--- src/learningjavabook/chapter1/HelloJava2.java
+++ src/learningjavabook/chapter1/HelloJava2.java
@@ -1,4 +1,13 @@
 package learningjavabook.chapter1;
 
+import javax.swing.*;
+
 public class HelloJava2 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("HelloJava2");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
 }
