71f92aaf0eab7abb7c78bc968beadcc093daeaf5
Miguel <migueljomartinez@gmail.com>
2023-09-26 20:40:29 -0500

    Moving HelloComponent2 to HelloJava2

--- src/learningjavabook/chapter1/HelloJava2.java
+++ src/learningjavabook/chapter1/HelloJava2.java
@@ -1,6 +1,9 @@
 package learningjavabook.chapter1;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 public class HelloJava2 {
 	public static void main(String[] args) {
@@ -12,4 +15,30 @@ public class HelloJava2 {
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 	}
+}
+
+class HelloComponent2 extends JComponent implements MouseMotionListener {
+	String message;
+	int messageX = 125;
+	int messageY = 95;
+
+	public HelloComponent2(String message) {
+		this.message = message;
+		this.addMouseMotionListener(this);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.drawString(message, messageX, messageY);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		this.messageX = e.getX();
+		this.messageY = e.getY();
+		this.repaint();
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {}
 }
