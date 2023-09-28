08dce5fdaff5d952d0546841fd3e12681fbab802
Miguel <migueljomartinez@gmail.com>
2023-09-27 20:10:25 -0500

    int, double, boolean

---
+++ src/learningjavabook/chapter4/variables.java
@@ -0,0 +1,9 @@
+package learningjavabook.chapter4;
+
+public class variables {
+	public static void main(String[] args) {
+		int foo = 42;
+		double d1 = 3.14, d2 = 2 * d1;
+		boolean isFun = true;
+	}
+}
