d2dc9d09aa6d35b0bc5b593c5903e9d2dc136480
Miguel <migueljomartinez@gmail.com>
2023-09-27 20:13:02 -0500

    Binary numbers

--- src/learningjavabook/chapter4/variables.java
+++ src/learningjavabook/chapter4/variables.java
@@ -5,5 +5,10 @@ public class variables {
 		int foo = 42;
 		double d1 = 3.14, d2 = 2 * d1;
 		boolean isFun = true;
+
+		int i = 1230;
+		// Binary numbers begin with 0b or 0B
+		int binary = 0b01001011; // 75 in decimal
+		//
 	}
 }
