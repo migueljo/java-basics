be36284c896be1ec22ba22cfef6f95f6a587c891
Miguel <migueljomartinez@gmail.com>
2023-09-30 07:56:34 -0500

    Enhanced for loop

---
+++ src/learningjavabook/chapter4/EnhancedForLoop.java
@@ -0,0 +1,15 @@
+package learningjavabook.chapter4;
+
+import java.util.ArrayList;
+
+public class EnhancedForLoop {
+	public static void main(String[] args) {
+		int [] intsArray = new int[] {10, 20, 30, 40, 50, 60};
+		for(int number: intsArray) System.out.println("Number: " + number);
+
+		ArrayList<Integer> intsArrayList = new ArrayList();
+		intsArrayList.add(70);
+		intsArrayList.add(80);
+		for(Integer number: intsArrayList) System.out.println("Number: " + number);
+	}
+}
