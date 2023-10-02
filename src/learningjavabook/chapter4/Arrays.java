39a00f1a27c37f0da29918bb60ba96d7057cee5c
Miguel <migueljomartinez@gmail.com>
2023-10-01 21:07:31 -0500

    Init more arrays

--- src/learningjavabook/chapter4/Arrays.java
+++ src/learningjavabook/chapter4/Arrays.java
@@ -1,8 +1,12 @@
 package learningjavabook.chapter4;
 
+import java.awt.*;
+
 public class Arrays {
 	public static void main(String[] args) {
 		int arrayOfInts[]; // c styled
-		int [] arrayOfInts2; // preferred
+		int[] arrayOfInts2; // preferred
+		String[] someArrays;
+		Button[] someButtons;
 	}
 }
