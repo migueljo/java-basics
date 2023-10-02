8f48d9c488a691bd06df7724814f830f1e89f1f1
Miguel <migueljomartinez@gmail.com>
2023-10-01 21:13:26 -0500

    Assigning to arrays

--- src/learningjavabook/chapter4/Arrays.java
+++ src/learningjavabook/chapter4/Arrays.java
@@ -6,7 +6,22 @@ public class Arrays {
 	public static void main(String[] args) {
 		int arrayOfInts[]; // c styled
 		int[] arrayOfInts2; // preferred
-		String[] someArrays;
+		String[] someStrings;
 		Button[] someButtons;
+
+		arrayOfInts = new int[42];
+		int number = 3;
+		someStrings = new String[number + 2];
+		double[] someNumbers = new double[23];
+		Component[] components = new Component[2];
+
+		for (int i = 0; i < someNumbers.length; i++) {
+			System.out.println("Double: " + someNumbers[i]);
+		}
+
+		String names [] = new String [4]; names [0] = new String();
+		names [1] = "Walla Walla";
+		names [2] = someStrings.toString();
+		// names[3] == null
 	}
 }
