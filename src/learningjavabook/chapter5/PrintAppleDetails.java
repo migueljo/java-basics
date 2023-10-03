3f809b220f2744acc56251033152a4ca8c57364e
Miguel <migueljomartinez@gmail.com>
2023-10-03 08:19:43 -0500

    Updating a1

--- src/learningjavabook/chapter5/PrintAppleDetails.java
+++ src/learningjavabook/chapter5/PrintAppleDetails.java
@@ -7,5 +7,13 @@ public class PrintAppleDetails {
 		System.out.println(" mass: " + a1.mass);
 		System.out.println(" diameter: " + a1.diameter);
 		System.out.println(" position: (" + a1.x + ", " + a1.y +")");
+
+		a1.mass = 10;
+		a1.x = 20;
+		a1.y = 40;
+		System.out.println("Updated a1:");
+		System.out.println("  mass: " + a1.mass);
+		System.out.println("  diameter: " + a1.diameter);
+		System.out.println("  position: (" + a1.x + ", " + a1.y +")");
 	}
 }
