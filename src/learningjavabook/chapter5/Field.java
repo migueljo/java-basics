b15e1ce57188a7ad5c48c5dc9e1ef4888e8b98dd
Miguel <migueljomartinez@gmail.com>
2023-10-03 08:30:55 -0500

    Adding basic structure to Field class

--- src/learningjavabook/chapter5/Field.java
+++ src/learningjavabook/chapter5/Field.java
@@ -1,4 +1,20 @@
 package learningjavabook.chapter5;
 
 public class Field {
+	Apple a1 = new Apple();
+	Apple a2 = new Apple();
+
+	public void setupApples() {
+		a1.diameter = 3.0f; a1.mass = 5.0f;
+		a1.x = 20;
+		a1.y = 40;
+		a2.diameter = 8.0f;
+		a2.mass = 10.0f;
+		a2.x = 70;
+		a2.y = 200;
+	}
+	public void detectCollisions() {
+		if (a1.isTouching(a2)) System.out.println("Collision detected: apples are touching!");
+		else System.out.println("Apples are not touching");
+	}
 }
