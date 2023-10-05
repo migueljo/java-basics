0a409389196879854fa620ab9eceafd81b3af162
Miguel <migueljomartinez@gmail.com>
2023-10-04 22:13:02 -0500

    Using 'this'

--- src/learningjavabook/chapter5/Apple.java
+++ src/learningjavabook/chapter5/Apple.java
@@ -2,12 +2,23 @@ package learningjavabook.chapter5;
 
 public class Apple {
 	final static float EARTH_ACCELERATION = 9.8f;
+	static int SMALL = 0, MEDIUM = 1, LARGE = 2;
 	float mass;
 	private float diameter = 1.0f;
 	int x, y;
 
 	float getWeight() {
 		return mass * EARTH_ACCELERATION;
+	}
+
+	public void moveTo(int x, int y) {
+		System.out.println("Moving apple to x = " + x + ", y = " + y);
+		this.x = x;
+		if (y > diameter / 2) {
+			this.y = y;
+		} else {
+			this.y = (int)(diameter / 2);
+		}
 	}
 
 	boolean isTouching(Apple other) {
