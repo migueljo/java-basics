c1197166774f212545346867f9c75e806b36f7b5
Miguel <migueljomartinez@gmail.com>
2023-10-09 10:04:47 -0500

    Implementing interface 3

--- src/learningjavabook/chapter5/Interfaces.java
+++ src/learningjavabook/chapter5/Interfaces.java
@@ -1,7 +1,8 @@
 package learningjavabook.chapter5;
 
 public class Interfaces {
-
+	Automobile automobile = new Automobile();
+	Lawnmower lawnmower = new Lawnmower();
 }
 
 interface Drivable {
@@ -23,6 +24,28 @@ class Automobile implements Drivable {
 	@Override
 	public void stopEngine() {
 		engineRunning = false;
+	}
+
+	@Override
+	public float accelerate(float acc) {
+		return 0;
+	}
+
+	@Override
+	public boolean turn(String dir) {
+		return false;
+	}
+}
+
+class Lawnmower implements Drivable {
+	@Override
+	public boolean startEngine() {
+		return false;
+	}
+
+	@Override
+	public void stopEngine() {
+
 	}
 
 	@Override
