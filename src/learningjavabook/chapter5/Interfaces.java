176f7a1627c570591801ba72abb514629e99f7c5
Miguel <migueljomartinez@gmail.com>
2023-10-09 09:50:14 -0500

    Implementing interface

--- src/learningjavabook/chapter5/Interfaces.java
+++ src/learningjavabook/chapter5/Interfaces.java
@@ -10,3 +10,25 @@ interface Drivable {
 	float accelerate(float acc);
 	boolean turn(String dir);
 }
+
+class Automobile implements Drivable {
+	@Override
+	public boolean startEnging() {
+		return false;
+	}
+
+	@Override
+	public void stopEngine() {
+
+	}
+
+	@Override
+	public float accelerate(float acc) {
+		return 0;
+	}
+
+	@Override
+	public boolean turn(String dir) {
+		return false;
+	}
+}
