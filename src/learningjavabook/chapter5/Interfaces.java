83c648a4c7345ccdb4d6e8e1d57d138a5d7b887c
Miguel <migueljomartinez@gmail.com>
2023-10-09 09:43:48 -0500

    Working with interfaces

---
+++ src/learningjavabook/chapter5/Interfaces.java
@@ -0,0 +1,12 @@
+package learningjavabook.chapter5;
+
+public class Interfaces {
+
+}
+
+interface Drivable {
+	boolean startEnging();
+	void stopEngine();
+	float accelerate(float acc);
+	boolean turn(String dir);
+}
