5e43ab0a1d0a8dea3c3ae260724d91b9f375fb17
Miguel <migueljomartinez@gmail.com>
2023-10-10 18:27:51 -0500

    Using inner classes part 2

---
+++ src/learningjavabook/chapter5/HelloJava3.java
@@ -0,0 +1,44 @@
+package learningjavabook.chapter5;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class HelloJava3 extends JFrame {
+	public static void main(String[] args) {
+		HelloJava3 demo = new HelloJava3();
+		demo.setVisible(true);
+	}
+
+	public HelloJava3() {
+		super("Hello Java 3");
+		HelloComponent3 text = new HelloComponent3("Hello, Inner Java!");
+		add(text);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 300);
+	}
+
+	class HelloComponent3 extends JComponent {
+		String theMessage;
+		int messageX = 125, messageY = 95;
+		public HelloComponent3(String message) {
+			theMessage = message;
+			addMouseMotionListener(new MouseMotionListener() {
+				@Override
+				public void mouseDragged(MouseEvent e) {
+					messageX = e.getX();
+					messageY = e.getY();
+					repaint();
+				}
+				@Override
+				public void mouseMoved(MouseEvent e) {}
+			});
+		}
+
+		@Override
+		public void paintComponents(Graphics g) {
+			g.drawString(theMessage, messageX, messageY);
+		}
+	}
+}
