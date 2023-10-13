00d20cbb47e5f63027231ba3a94ed690509e560f
Miguel <migueljomartinez@gmail.com>
2023-10-12 21:19:16 -0500

    ParseException

--- src/learningjavabook/chapter6/Exceptions.java
+++ src/learningjavabook/chapter6/Exceptions.java
@@ -2,3 +2,18 @@ package learningjavabook.chapter6;
 
 public class Exceptions {
 }
+
+class ParseException extends Exception {
+	private int lineNumber;
+	ParseException() {
+		super();
+		this.lineNumber = -1;
+	}
+	ParseException(String description, int lineNumber) {
+		super(description);
+		this.lineNumber = lineNumber;
+	}
+	public int getLineNumber() {
+		return lineNumber;
+	}
+}
