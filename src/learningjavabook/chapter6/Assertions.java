366e953682bc7d8c310bc6204a5828d5e0e5840d
Miguel <migueljomartinez@gmail.com>
2023-10-13 09:04:44 -0500

    Assertions

---
+++ src/learningjavabook/chapter6/Assertions.java
@@ -0,0 +1,10 @@
+package learningjavabook.chapter6;
+
+public class Assertions {
+	/**
+	 *  assert false;
+	 * 	assert array.length > min;
+	 * 	assert a > 0 : a // shows value of "a" to the user
+	 * 	assert foo != null : "foo is null" // shows message "foo is null" to the user
+	 */
+}
