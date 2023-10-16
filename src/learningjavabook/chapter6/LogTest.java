9ca3e24cb80f21dce521745a9e66910015594d85
Miguel <migueljomartinez@gmail.com>
2023-10-15 22:10:42 -0500

    Log types

--- src/learningjavabook/chapter6/LogTest.java
+++ src/learningjavabook/chapter6/LogTest.java
@@ -5,5 +5,12 @@ import java.util.logging.Logger;
 public class LogTest {
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger("learningjavabook.chapter6.LogTest");
+		logger.severe("Power lost - running on backup!");
+		logger.warning("Database connection lost, retrying...");
+		logger.info("Startup complete.");
+		logger.config("Server configuration: standalone, JVM version 1.5");
+		logger.fine("Loading graphing package.");
+		logger.finer("Doing pie chart");
+		logger.finest("Starting bubble sort: value = " + 42);
 	}
 }
