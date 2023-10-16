d060cf4b290cbd529e44d8be518e65a31991a2ef
Miguel <migueljomartinez@gmail.com>
2023-10-15 22:08:28 -0500

    LogTest class

---
+++ src/learningjavabook/chapter6/LogTest.java
@@ -0,0 +1,9 @@
+package learningjavabook.chapter6;
+
+import java.util.logging.Logger;
+
+public class LogTest {
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger("learningjavabook.chapter6.LogTest");
+	}
+}
