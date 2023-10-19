4e84c1ad581312cf1d8505985e20574c605bc676
Miguel <migueljomartinez@gmail.com>
2023-10-19 08:21:25 -0500

    Generics part 2

--- src/learningjavabook/chapter7/Generics.java
+++ src/learningjavabook/chapter7/Generics.java
@@ -1,13 +1,26 @@
 package learningjavabook.chapter7;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Generics {
 	public static void main(String[] args) {
 		List<String> listOfStrings = new ArrayList<>();
 		listOfStrings.add("Hello");
-
 		String number1 = listOfStrings.get(0);
+
+		//////////////////////
+		Map<Integer, Employee> employeesMap = new HashMap<>();
+		Employee bob = new Employee();
+		Integer bobsId = 314; // This is autoboxing :o
+		employeesMap.put(bobsId, bob);
+
+		System.out.println(employeesMap);
 	}
+}
+
+class Employee {
+	String name = "Employee name";
 }
