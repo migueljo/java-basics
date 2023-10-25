925c6e312b97dfb95a3c1bfd8556672427b7526a
Miguel <migueljomartinez@gmail.com>
2023-10-25 18:52:27 -0500

    Strings part 1

--- src/learningjavabook/chapter8/Strings.java
+++ src/learningjavabook/chapter8/Strings.java
@@ -1,5 +1,9 @@
 package learningjavabook.chapter8;
 
 public class Strings {
-
+	public static void main(String[] args) {
+		String quote = "To be or not to be";
+		int quoteLength = quote.length();
+		boolean isQuoteEmpty = quote.isEmpty();
+	}
 }
