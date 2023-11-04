4029332d4983354f63577d144849e7f5cfef3989
Miguel <migueljomartinez@gmail.com>
2023-11-03 23:09:02 -0500

    Regexp part 9

--- src/learningjavabook/chapter8/RegExp.java
+++ src/learningjavabook/chapter8/RegExp.java
@@ -27,6 +27,7 @@ public class RegExp {
 	static void customClasses() {
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
 		String numbers = "0123456789";
+		String arose = "A rose is a rose is a rose";
 		String allLettersAndNumbers = alphabet + numbers;
 		System.out.println("First 5 letters: " + Arrays.toString(alphabet.split("[abcde]")));
 		System.out.println("Match all letters: " + Arrays.toString(alphabet.split("[a-z]")));
@@ -35,5 +36,7 @@ public class RegExp {
 		System.out.println("Match al numbers after 5 to 9: " + Arrays.toString(numbers.split("[^0-5]")));
 		System.out.println("Composing range and 1 character: " + Arrays.toString(numbers.split("[0-59]")));
 		System.out.println("Logical && AND: " + Arrays.toString(alphabet.split("[a-z&&[^m-p]]"))); // Simplify warning works in another way
+		System.out.println("Match at the beginning: " + Arrays.toString(arose.split("^[Aa] rose")));
+		System.out.println("Match at the end: " + Arrays.toString(arose.split("[Aa] rose$")));
 	}
 }
