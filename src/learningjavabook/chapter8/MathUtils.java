9be50a8adf34848455b6acbcf743a2d251d42b3a
Miguel <migueljomartinez@gmail.com>
2023-11-07 19:50:30 -0500

    Math utils part 2

--- src/learningjavabook/chapter8/MathUtils.java
+++ src/learningjavabook/chapter8/MathUtils.java
@@ -1,5 +1,9 @@
 package learningjavabook.chapter8;
 
 public class MathUtils {
-
+	public static void main(String[] args) {
+		System.out.println(Math.signum(-12));
+		System.out.println(Math.signum(12));
+		System.out.println(Math.signum(0));
+	}
 }
