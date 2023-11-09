c06d5a493a54513ba5bfcf949d8d0d6edc5e8245
Miguel <migueljomartinez@gmail.com>
2023-11-09 17:33:29 -0500

    Date time part 1

--- src/learningjavabook/chapter8/DateAndTime.java
+++ src/learningjavabook/chapter8/DateAndTime.java
@@ -1,5 +1,17 @@
 package learningjavabook.chapter8;
 
-public class DateAndTime {
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+public class DateAndTime {
+	public static void main(String[] args) {
+		LocalDate d0 = LocalDate.of(2019, 11, 23);
+		System.out.println(d0);
+		LocalDate d1 = LocalDate.parse("2019-11-24");
+		System.out.println(d1);
+		LocalTime t1 = LocalTime.of(23,59);
+		System.out.println(t1);
+		LocalTime t2 = LocalTime.parse("23:58");
+		System.out.println(t2);
+	}
 }
