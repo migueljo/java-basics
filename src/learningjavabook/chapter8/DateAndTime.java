e93a35ce015278ee2fd79bc70ec9a4dbeebe4401
Miguel <migueljomartinez@gmail.com>
2023-11-09 17:44:33 -0500

    Date time part 4

--- src/learningjavabook/chapter8/DateAndTime.java
+++ src/learningjavabook/chapter8/DateAndTime.java
@@ -3,10 +3,25 @@ package learningjavabook.chapter8;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class DateAndTime {
 	public static void main(String[] args) {
+		basicDateAndTimes();
+		comparingAndManipulating();
+	}
+
+	static void comparingAndManipulating() {
+		LocalDateTime dt0 = LocalDateTime.parse("2000-01-01T00:00");
+		LocalDateTime dt1 = LocalDateTime.now();
+
+		System.out.println("Seconds: " + ChronoUnit.SECONDS.between(dt0, dt1));
+		System.out.println("Days: " + ChronoUnit.DAYS.between(dt0, dt1));
+		System.out.println("Years: " + ChronoUnit.YEARS.between(dt0, dt1));
+	}
+
+	static void basicDateAndTimes() {
 		LocalDate d0 = LocalDate.of(2019, 11, 23);
 		System.out.println(d0);
 		LocalDate d1 = LocalDate.parse("2019-11-24");
