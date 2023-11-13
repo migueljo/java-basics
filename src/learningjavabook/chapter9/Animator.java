3e2776079c2d27b5dda36ff0797f9366731f2bfe
Miguel <migueljomartinez@gmail.com>
2023-11-13 14:30:38 -0500

    Threads part 3

--- src/learningjavabook/chapter9/Animator.java
+++ src/learningjavabook/chapter9/Animator.java
@@ -4,10 +4,21 @@ public class Animator implements Runnable {
 	boolean animating = true;
 	@Override
 	public void run() {
-		while (animating) {
+		Integer i = 0;
+		while (i < 1000) {
 			// move apples one frame
 			// repaint
 			// pause
+			System.out.println("I is: " + i);
+			i++;
 		}
+	}
+}
+
+class Hilo {
+	public static void main(String[] args) {
+		Animator animator = new Animator();
+		Thread thread = new Thread(animator);
+		thread.start();
 	}
 }
