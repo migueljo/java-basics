1acdbf8d80cb28e33110f1a915efe9cab95e8757
Miguel <migueljomartinez@gmail.com>
2023-11-13 14:26:32 -0500

    Threads part 2

--- src/learningjavabook/chapter9/Animator.java
+++ src/learningjavabook/chapter9/Animator.java
@@ -1,8 +1,13 @@
 package learningjavabook.chapter9;
 
-public class Animator implements Runnable{
+public class Animator implements Runnable {
+	boolean animating = true;
 	@Override
 	public void run() {
-
+		while (animating) {
+			// move apples one frame
+			// repaint
+			// pause
+		}
 	}
 }
