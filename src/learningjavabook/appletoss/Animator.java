cbf9e2eee4a9d58ea92ee4ce70e74e4b5ccbe5b3
Miguel <migueljomartinez@gmail.com>
2023-11-14 21:18:56 -0500

    WIP: Animator class

--- src/learningjavabook/appletoss/Animator.java
+++ src/learningjavabook/appletoss/Animator.java
@@ -1,16 +1,28 @@
 package learningjavabook.appletoss;
 
 public class Animator implements Runnable {
+	public static final int STEP = 40; // duration of an animation frame in milliseconds
 	boolean animating = true;
 	@Override
 	public void run() {
-		int i = 0;
-		while (i < 1000) {
-			// move apples one frame
-			// repaint
-			// pause
-			System.out.println("I is: " + i);
-			i++;
+		while (animating) {
+			// "animating" is a global variable that allows us to stop animating
+			// and conserve resources if there are no active apples to move
+			while (animating) {
+				System.out.println("Stepping " + apples.size() + " apples");
+				for (Apple a : apples) {
+					a.step();
+					detectCollisions(a);
+				}
+				Field.this.repaint();
+				cullFallenApples();
+				try {
+					Thread.sleep((int)(STEP * 1000));
+				} catch (InterruptedException ie) {
+					System.err.println("Animation interrupted");
+					animating = false;
+				}
+			}
 		}
 	}
 }
