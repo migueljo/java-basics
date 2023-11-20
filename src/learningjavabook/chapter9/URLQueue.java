ac2fc8179cf867e79113b47ee695a550d65b895c
Miguel <migueljomartinez@gmail.com>
2023-11-19 20:00:12 -0500

    addUrl()

--- src/learningjavabook/chapter9/URLQueue.java
+++ src/learningjavabook/chapter9/URLQueue.java
@@ -1,4 +1,10 @@
 package learningjavabook.chapter9;
 
+import java.util.LinkedList;
+
 public class URLQueue {
+	LinkedList<String> urlQueue = new LinkedList<>();
+	public synchronized void addUrl(String url) {
+		urlQueue.add(url);
+	}
 }
