26eb8b44228f6222182dceb8ab8072aa00974a2a
Miguel <migueljomartinez@gmail.com>
2023-11-19 20:01:28 -0500

    getURL()

--- src/learningjavabook/chapter9/URLQueue.java
+++ src/learningjavabook/chapter9/URLQueue.java
@@ -4,7 +4,14 @@ import java.util.LinkedList;
 
 public class URLQueue {
 	LinkedList<String> urlQueue = new LinkedList<>();
-	public synchronized void addUrl(String url) {
+	public synchronized void addURL(String url) {
 		urlQueue.add(url);
+	}
+
+	public synchronized String getURL() {
+		if (!urlQueue.isEmpty()) {
+			return urlQueue.removeFirst();
+		}
+		return null;
 	}
 }
