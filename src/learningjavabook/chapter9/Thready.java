0361e9eb23f7f51279835dab85c9e82c50694f4b
Miguel <migueljomartinez@gmail.com>
2023-11-21 08:54:02 -0500

    Thread priority

--- src/learningjavabook/chapter9/Thready.java
+++ src/learningjavabook/chapter9/Thready.java
@@ -3,18 +3,24 @@ package learningjavabook.chapter9;
 
 public class Thready {
 	public static void main( String args [] ) {
-		new ShowThread("Foo").start();
-		new ShowThread("Bar").start();
+		Thread foo = new ShowThread("Foo");
+		Thread bar = new ShowThread("Bar");
+		foo.setPriority(Thread.MIN_PRIORITY);
+		bar.setPriority(Thread.MAX_PRIORITY);
+		foo.start();
+		bar.start();
 	}
 
 	static class ShowThread extends Thread {
 		String message;
+		int counter = 0;
 		ShowThread(String message) {
 			this.message = message;
 		}
 		public void run() {
 			while (true) {
-				System.out.println(message);
+				System.out.println(message + ": " + counter);
+				counter++;
 			}
 		}
 	}
