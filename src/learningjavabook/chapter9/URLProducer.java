e2b77371e267456c7da9c01a6525643eb7ad23e5
Miguel <migueljomartinez@gmail.com>
2023-11-20 20:19:19 -0500

    URLProducer.run()

--- src/learningjavabook/chapter9/URLProducer.java
+++ src/learningjavabook/chapter9/URLProducer.java
@@ -1,6 +1,24 @@
 package learningjavabook.chapter9;
 
-public class URLProducer {
-	URLQueue queue = new URLQueue();
+import java.util.Random;
 
+public class URLProducer {
+	String producerID;
+	int urlCount;
+	URLQueue queue;
+	Random delay;
+
+	public void run() {
+		for (int i = 0; i < urlCount; i++) {
+			String url = "https://some.url/at/path/" + i;
+			queue.addURL(producerID + " " + url);
+			System.out.println(producerID + " produced " + url);
+			try {
+				Thread.sleep(delay.nextInt(500));
+			} catch (InterruptedException e) {
+				System.err.println("Producer " + producerID + " interrupted. Quitting");
+				break;
+			}
+		}
+	}
 }
