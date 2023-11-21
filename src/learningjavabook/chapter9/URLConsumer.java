045d38dcdccb53f57923af3b4c9b11cacafada9f
Miguel <migueljomartinez@gmail.com>
2023-11-20 20:27:04 -0500

    URLConsumer constructor

--- src/learningjavabook/chapter9/URLConsumer.java
+++ src/learningjavabook/chapter9/URLConsumer.java
@@ -1,4 +1,20 @@
 package learningjavabook.chapter9;
 
+import java.util.Random;
+
 public class URLConsumer extends Thread{
+	String consumerID;
+	URLQueue queue;
+	boolean keepWorking;
+	Random delay;
+
+	URLConsumer(String id, URLQueue queue) {
+		if (queue == null) {
+			throw new IllegalArgumentException("Shared queue must not be null");
+		}
+		consumerID = id;
+		this.queue = queue;
+		keepWorking = true;
+		delay = new Random();
+	}
 }
