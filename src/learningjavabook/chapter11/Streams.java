2a3265c3f80080341869236f18e2f87ff91a8f3f
Miguel <migueljomartinez@gmail.com>
2023-11-23 18:35:44 -0500

    Streams part 1

--- src/learningjavabook/chapter11/Streams.java
+++ src/learningjavabook/chapter11/Streams.java
@@ -1,5 +1,16 @@
 package learningjavabook.chapter11;
 
-public class Streams {
+import java.io.InputStream;
+import java.io.OutputStream;
 
+public class Streams {
+	public static void main(String[] args) {
+		basicIO();
+	}
+
+	static void basicIO() {
+		InputStream stdin = System.in;
+		OutputStream stdout = System.out;
+		OutputStream stderr = System.err;
+	}
 }
