4d9266b4b2f14ea80bb3e0ed62bc91f3eb95803a
Miguel <migueljomartinez@gmail.com>
2023-12-06 17:53:39 -0500

    Streams part 14

--- src/learningjavabook/chapter11/FileStreams.java
+++ src/learningjavabook/chapter11/FileStreams.java
@@ -1,13 +1,14 @@
 package learningjavabook.chapter11;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileStreams {
 	public static void main(String[] args) {
-		try {
-			FileInputStream in = new FileInputStream("/tmp/passwd");
-		} catch(FileNotFoundException exception) {
+		// This try block is called try-with-resources
+		try (FileInputStream fileInputStream = new FileInputStream("/tmp/passwd")) {
+			System.out.println(fileInputStream);
+		} catch(IOException exception) {
 			System.err.println(exception);
 		}
 	}
