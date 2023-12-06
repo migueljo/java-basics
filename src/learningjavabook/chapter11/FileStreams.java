81c08e7715c2b200a1c6b16b7b46cbe8348ace69
Miguel <migueljomartinez@gmail.com>
2023-12-06 17:51:29 -0500

    Streams part 13

--- src/learningjavabook/chapter11/FileStreams.java
+++ src/learningjavabook/chapter11/FileStreams.java
@@ -1,7 +1,14 @@
 package learningjavabook.chapter11;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class FileStreams {
 	public static void main(String[] args) {
-
+		try {
+			FileInputStream in = new FileInputStream("/tmp/passwd");
+		} catch(FileNotFoundException exception) {
+			System.err.println(exception);
+		}
 	}
 }
