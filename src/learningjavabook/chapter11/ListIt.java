c0faeb93a9a8a5629ecef6e29511a9aa6bf0e710
Miguel <migueljomartinez@gmail.com>
2023-12-07 14:51:37 -0500

    Streams part 16

--- src/learningjavabook/chapter11/ListIt.java
+++ src/learningjavabook/chapter11/ListIt.java
@@ -1,7 +1,32 @@
 package learningjavabook.chapter11;
 
+import java.io.*;
+
 public class ListIt {
 	public static void main(String[] args) {
+		File file = new File(args[0]);
 
+		if (!file.exists() || !file.canRead()) {
+			System.err.println("Can't read" + file);
+			return;
+		}
+
+		if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			for (File f: files) {
+				System.out.println(f);
+			}
+		} else {
+			try {
+				Reader inputStreamReader = new InputStreamReader(new FileInputStream(file));
+				BufferedReader in = new BufferedReader(inputStreamReader);
+				String line;
+				while ((line = in.readLine()) != null) {
+					System.out.println(line);
+				}
+			} catch(IOException e) {
+				System.err.println("File error:" + e);
+			}
+		}
 	}
 }
