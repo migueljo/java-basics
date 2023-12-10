667da69196414a8579f9b9428e6e4107b42717da
Miguel <migueljomartinez@gmail.com>
2023-12-10 13:36:43 -0500

    Streams part 22

--- src/learningjavabook/chapter11/nio/NIOApi.java
+++ src/learningjavabook/chapter11/nio/NIOApi.java
@@ -1,7 +1,11 @@
 package learningjavabook.chapter11.nio;
 
-public class FileSystemAndPath {
+public class NIOApi {
 	public static void main(String[] args) {
+		fileSystemAndPath();
+	}
+
+	static void fileSystemAndPath() {
 
 	}
 }
