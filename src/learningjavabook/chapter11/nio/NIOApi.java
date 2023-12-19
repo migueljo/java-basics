8b9837e1c2458d57d3d00e25dce4b4909fc2bafc
Miguel <migueljomartinez@gmail.com>
2023-12-18 19:21:53 -0500

    Streams part 24

--- src/learningjavabook/chapter11/nio/NIOApi.java
+++ src/learningjavabook/chapter11/nio/NIOApi.java
@@ -1,9 +1,12 @@
 package learningjavabook.chapter11.nio;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +21,8 @@ public class NIOApi {
 			Map<String, String> props = new HashMap<>();
 			URI zipUri = URI.create("jar:file:/Users/mmartinez/tmp/MyArchive.zip");
 			FileSystem zipfs = FileSystems.newFileSystem(zipUri, props);
+			Path fooPath = fs.getPath("/tmp/foo.txt");
+			OutputStream out = Files.newOutputStream(fooPath);
 		} catch (IOException e) {
 			System.err.println(e);
 		}
